c33d18fdfba0cefc9d15ca065163834d05249f66
Enrico Magboo <enricomagboo@yahoo.com>
2017-06-09 00:48:04 +0800

    Commit: Working: using default checkboxfor

---
+++ MultiswitchDemo/Controllers/RoomController.cs
@@ -0,0 +1,27 @@
+﻿using MultiswitchDemo.Models;
+using System.Web.Mvc;
+
+namespace MultiswitchDemo.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: Room
+        public ActionResult Create()
+        {
+            return View(new Room());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Room model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //do stuff
+
+            return View(model);
+        }
+    }
+}
---
+++ MultiswitchDemo/Models/Room.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiswitchDemo.Models
+{
+    public class Room
+    {
+        [Display(Name = "Room Number")]
+        public int RoomNumber { get; set; }
+
+        [Display(Name = "Room Name")]
+        public string RoomName { get; set; }
+
+        [Display(Name = "Is reserved?")]
+        public bool IsReserved { get; set; }
+    }
+}
